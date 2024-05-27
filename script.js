0131c747a2fa0151df15b605d6da801e6ae5884b
Eduardo Gil <egpheel@users.noreply.github.com>
2024-05-27 14:53:53 +0100

    display now updates according to the buttons pressed. also added clear display functionality

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+const clearButton = document.querySelector(".calc-clear-button");
+const calcButtons = document.querySelectorAll(".calc-button");
+const display = document.querySelector(".display");
+
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
 let displayValue = "";
+
+calcButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    handleButtons(e.target.textContent);
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  clearDisplay();
+});
+
+const handleButtons = (buttonText) => {
+  displayValue += buttonText;
+  updateDisplay();
+};
+
+const clearDisplay = () => {
+  displayValue = "";
+  updateDisplay();
+};
+
+const updateDisplay = () => {
+  display.textContent = displayValue;
+};
 
 const operate = (a, b, op) => {
   switch (op) {
